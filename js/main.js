6ea512471eaca50236a8f66058f7df15da8e34c9
vitaliykiktev <vitaliykiktev@yandex.ru>
2022-11-28 15:37:31 +0300

    add: first task

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function getRandomPositiveInteger (a, b = 1) {
+  if (a === undefined) {
+    throw new Error('Первый параметр должен быть число');
+  }
+
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+getRandomPositiveInteger(10, 20);
+
+
+const checkStringLength = (str, lengthCount) => str.length <= lengthCount;
+checkStringLength();
